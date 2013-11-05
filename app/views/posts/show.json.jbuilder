555a455001c5e844afbd6e69de3bf5047b2841a0
YudaiNoguchi <yudainoguchi19@gmail.com>
2013-11-05 18:04:16 +0900

    railsプロジェクトの作成

---
+++ app/views/posts/show.json.jbuilder
@@ -0,0 +1,1 @@
+json.extract! @post, :user, :body, :created_at, :updated_at
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Firstapp::Application.config.secret_key_base = '84e3a8ae3db3b2e1aa316be1f245662fd822051cebada420175b606b426d7b2c764f406a6fd44679fbbb0bec23674a2dc2cdcf91522f3f35f4f1e2286a7d47da'
